5701937558a16897eda080b5e6145e9673ab8368
Jerry W <jerrywiltse@gmail.com>
2017-08-14 21:39:14 -0400

    mass update - minor recipe and cosmetic readme

--- conanfile.py
+++ conanfile.py
@@ -3,8 +3,8 @@ from conans import ConanFile, tools, os
 class BoostLexical_CastConan(ConanFile):
     name = "Boost.Lexical_Cast"
     version = "1.64.0"
-    generators = "txt"
-    url = "https://github.com/boostorg/lexical_cast"
+    url = "https://github.com/bincrafters/conan-boost-lexical_cast"
+    source_url = "https://github.com/boostorg/lexical_cast"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_name = "lexical_cast"
@@ -26,7 +26,7 @@ class BoostLexical_CastConan(ConanFile):
 
     def source(self):
         self.run("git clone --depth=50 --branch=boost-{0} {1}.git"
-                 .format(self.version, self.url))
+                 .format(self.version, self.source_url))
 
     def package(self):
         include_dir = os.path.join(self.build_folder, self.lib_short_name, "include")
