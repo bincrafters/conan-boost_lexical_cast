b36d7ecf71b3cb8c20d23d64a9c51e460b4bbb7d
Jerry W <jerrywiltse@gmail.com>
2017-08-20 00:43:49 -0400

    enabled short paths

--- conanfile.py
+++ conanfile.py
@@ -3,6 +3,7 @@ from conans import ConanFile, tools, os
 class BoostLexical_CastConan(ConanFile):
     name = "Boost.Lexical_Cast"
     version = "1.64.0"
+    short_paths = True
     url = "https://github.com/bincrafters/conan-boost-lexical_cast"
     source_url = "https://github.com/boostorg/lexical_cast"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
@@ -26,7 +27,7 @@ class BoostLexical_CastConan(ConanFile):
 
     def source(self):
         for lib_short_name in self.lib_short_names:
-            self.run("git clone --depth=50 --branch=boost-{0} https://github.com/boostorg/{1}.git"
+            self.run("git clone --depth=1 --branch=boost-{0} https://github.com/boostorg/{1}.git"
                      .format(self.version, lib_short_name)) 
 
     def package(self):
