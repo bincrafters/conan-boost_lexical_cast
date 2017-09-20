451b3df1e016eefe9a1faf41a2b28d855dc94008
Jerry W <jerrywiltse@gmail.com>
2017-09-20 02:19:11 -0400

    fixed erroneous recipes for group members

--- conanfile.py
+++ conanfile.py
@@ -7,35 +7,15 @@ class BoostLexical_CastConan(ConanFile):
     url = "https://github.com/bincrafters/conan-boost-lexical_cast"
     description = "Please visit http://www.boost.org/doc/libs/1_65_1/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
-    lib_short_names = ["lexical_cast"]
-    requires =  "Boost.Array/1.65.1@bincrafters/testing", \
-                      "Boost.Assert/1.65.1@bincrafters/testing", \
-                      "Boost.Config/1.65.1@bincrafters/testing", \
-                      "Boost.Container/1.65.1@bincrafters/testing", \
-                      "Boost.Core/1.65.1@bincrafters/testing", \
-                      "Boost.Integer/1.65.1@bincrafters/testing", \
-                      "Boost.Math/1.65.1@bincrafters/testing", \
-                      "Boost.Mpl/1.65.1@bincrafters/testing", \
-                      "Boost.Numeric_Conversion/1.65.1@bincrafters/testing", \
-                      "Boost.Range/1.65.1@bincrafters/testing", \
-                      "Boost.Static_Assert/1.65.1@bincrafters/testing", \
-                      "Boost.Throw_Exception/1.65.1@bincrafters/testing", \
-                      "Boost.Type_Traits/1.65.1@bincrafters/testing"
-
-                      #array3 assert1 config0 container7 core2 integer3 math8 mpl5 numeric~conversion6 range7 static_assert1 throw_exception2 type_traits3
-
-    def source(self):
-        boostorg_github = "https://github.com/boostorg"
-        archive_name = "boost-" + self.version  
-        for lib_short_name in self.lib_short_names:
-            tools.get("{0}/{1}/archive/{2}.tar.gz"
-                .format(boostorg_github, lib_short_name, archive_name))
-            os.rename(lib_short_name + "-" + archive_name, lib_short_name)
-
-    def package(self):
-        for lib_short_name in self.lib_short_names:
-            include_dir = os.path.join(lib_short_name, "include")
-            self.copy(pattern="*", dst="include", src=include_dir)
-
+    requires =  "Boost.Level8Group/1.65.1@bincrafters/testing"
+    
     def package_id(self):
         self.info.header_only()
+        
+    #This library is part of one or more cyclic dependency groups within Boost.
+    
+    #All members of cyclic dependency groups must be built under single package per group for Conan.
+    
+    #The combination is performed in the package(s) listed in the requires field.
+    
+    #This package enables simple consumption of this library while abstracting away the cyclic dependency issues. 
