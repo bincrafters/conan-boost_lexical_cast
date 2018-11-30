6e3add7f1588244c13f2ce23bba277aa954b6866
Rene Rivera <grafikrobot@gmail.com>
2018-11-29 22:01:12 -0600

    Initial 1.68.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,16 +4,12 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.67.0@bincrafters/testing")
+base = python_requires("boost_base/1.68.0@bincrafters/testing")
 
 class BoostLexical_CastConan(base.BoostBaseConan):
     name = "boost_lexical_cast"
     url = "https://github.com/bincrafters/conan-boost_lexical_cast"
     lib_short_names = ["lexical_cast"]
     header_only_libs = ["lexical_cast"]
-    cycle_group = "boost_level8group"
-    b2_requires = [
-        "boost_level8group",
-    ]
-
-
+    cycle_group = "boost_cycle_group_b"
+    b2_requires = ["boost_cycle_group_b"]
