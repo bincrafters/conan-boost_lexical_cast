d910c0af17dce1c7e080699939d90ca9f5b55e56
Jerry W <jerrywiltse@gmail.com>
2019-01-26 23:58:54 -0500

    re-implement version numbers, fix travis badges, refresh ci scripts, move to python 3.6 on ci

--- conanfile.py
+++ conanfile.py
@@ -8,6 +8,7 @@ base = python_requires("boost_base/1.68.0@bincrafters/testing")
 
 class BoostLexical_CastConan(base.BoostBaseConan):
     name = "boost_lexical_cast"
+    version = "1.68.0"
     url = "https://github.com/bincrafters/conan-boost_lexical_cast"
     lib_short_names = ["lexical_cast"]
     header_only_libs = ["lexical_cast"]
